e8294a3615b33e89aca1eef03aec7badfcee0753
lanfei422 <whu2010feiba@gmail.com>
2016-11-17 23:11:29 +0800

    Update SBFL.py

--- SBFL.py
+++ SBFL.py
@@ -53,39 +53,69 @@ class Ochiai2(Formula):
         return susp_score
 
 class CalculateSBFL:
-    matrix={}
-    spectra={}
-    suspicious_value={}
-    test_case_pof={}
-    pof_tuple={}
+    version=-1
 
-    def load(self,url_m,url_s):
+    # matrix={}   #pass or fail info ,each row is a test case.
+    # suspicious_value={} #store entity's suspicious value
+    # test_case_pof={}    #indicate which case is failed.
+    # pof_tuple={}    #aef,aep,anf,anp
+    # suspicious_value_remain={}  #bias suspiciousness
+
+    spectra={}  #map every item in matrix to source code line.
+
+    @classmethod
+    def loadSpectra(url):
+        spectra_file = open(url, 'r')
+        for (lineNo, line) in enumerate(spectra_file):
+            CalculateSBFL.spectra[lineNo] = line
+
+    def __init__(self,version):
+        self.version=version
+
+    def load(self,url_m):
         matrix_file=open(url_m,'r')
-        spectra_file=open(url_s,'r')
-        for (lineNo,line) in enumerate(spectra_file):
-            self.spectra[lineNo]=line
-
         for (case,line) in enumerate(matrix_file):
             pass_fail_info=line.split(' ')
             self.test_case_pof[case]=pass_fail_info[-1]
             self.matrix=pass_fail_info[0:-1]
 
-        for key in self.spectra.keys:
-            self.pof_tuple[key]=[0,0,0,0] #aef,aep,anf,anp
-            for ts_key in self.test_case_pof.keys:
-                if self.test_case_pof.keys[ts_key]=='+':
-                    if self.spectra[key]=='0':
-                        self.pof_tuple[key][3]+=1
-                    else:
-                        self.pof_tuple[key][1]+=1
-                else:
-                    if self.spectra[key]=='0':
-                        self.pof_tuple[key][2]+=1
-                    else:
-                        self.pof_tuple[key][0]+=1
+        self.pof_tuple=self.genTuple(CalculateSBFL.spectra,self.test_case_pof)
 
-    def diffTestSuite(self,):
-        print ""
+    def genTuple(self,spectra,testInfo):
+        tuples={}
+        for key in spectra.keys():
+            tuples[key]=[0,0,0,0]
+            for ts_key in testInfo.keys:
+                if testInfo[ts_key]=='+':
+                    if spectra[key]=='0':
+                        tuples[key][3]+=1
+                    else:
+                        tuples[key][1]+=1
+                else:
+                    if spectra[key]=='0':
+                        tuples[key][2]+=1
+                    else:
+                        tuples[key][0]+=1
+        return tuples
+
+    def diffTestSuite(self,cbfl):
+        test_case_remain=[]
+        cur_test_case=self.test_case_pof
+        other_test_case=cbfl.test_case_pof
+        for (ct,ot) in zip(cur_test_case.items(),other_test_case.items()):
+            if ct[1]=='-' and ct[0]==ot[0] and ct[1]==ot[1]:
+                continue
+            test_case_remain.append(ot)
+        tmp={}
+        for (key,val) in test_case_remain:
+            tmp[key]=val
+
+        remainTuples=self.genTuple(CalculateSBFL.spectra,tmp)
+        sus_value={}
+        for (k,v) in remainTuples:
+            sus_value[k]=self.calculate(v)
+        self.suspicious_value_remain[cbfl.version]=sus_value
+
     def calculate(self,tuple):
         wong=Wong2(tuple)
         op2=Op2(tuple)
